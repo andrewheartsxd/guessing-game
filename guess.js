16160e424419ee108c12c5425d3b51aaf7b8e91a
Andrew <hwang.andrew@gmail.com>
2014-08-08 16:27:20 -0700

    still debugging

--- guess.js
+++ guess.js
@@ -1,13 +1,14 @@
 var balanceOne = 0;
 var numberOfRollsOne = 0;
 var balanceTwo = 0;
+var numberOfRollsTwo= 0;
 
-var rollDiceOne = function() {
-	var displayBalance = document.getElementById("balanceOne");
-	var displayRolls = document.getElementById("numberOfRollsOne");
-	numberOfRollsOne = numberOfRollsOne + 1
-	var dice1 = Math.floor((Math.random()*6)+1)
-	var dice2 = Math.floor((Math.random()*6)+1)
+function rollDiceOne() {
+	var displayBalanceOne = document.getElementById("balanceOne");
+	var displayRollsOne = document.getElementById("numberOfRollsOne");
+	numberOfRollsOne = numberOfRollsOne + 1;
+	var dice1 = Math.floor((Math.random()*6)+1);
+	var dice2 = Math.floor((Math.random()*6)+1);
 
 	if (dice1 + dice2 === 7 || dice1 + dice2 === 11) {
 		balanceOne = balanceOne + 1;
@@ -15,32 +16,48 @@ var rollDiceOne = function() {
 		balanceOne = balanceOne -1;
 	};
 
-	displayBalance.innerHTML = balanceOne;
-	displayRolls.innerHTML = numberOfRollsOne;
+	displayBalanceOne.innerHTML = balanceOne;
+	displayRollsOne.innerHTML = numberOfRollsOne;
 };
 
-var rollDiceTwo = function() {
-		if (numberOfRollsTwo === 0) {
-		alert("You're no fun!")
-	} else {
-		for (i = 1, i < document.getElementById("numberOfRollsTwo"), i++) {
-				var displayBalance = document.getElementById("balanceTwo");
-				var displayRolls = document.getElementById("numberOfRollsTwo");
-				numberOfRollsTwo = numberOfRollsOne - 1
-				var dice1 = Math.floor((Math.random()*6)+1)
-				var dice2 = Math.floor((Math.random()*6)+1)
 
-				if (dice1 + dice2 === 7 || dice1 + dice2 === 11) {
-					balanceTwo = balanceTwo + 1;
-				} else if (dice1 + dice2 === 2) {
-					balanceTwo = balanceTwo -1;
-				};
 
-				displayBalance.innerHTML = balanceTwo;
-				displayRolls.innerHTML = numberOfRollsTwo;
-		}
+function rollDiceTwo() {
+
+	var numberOfRollsOne = document.getElementById("numberOfRollsOne")
+
+	while (numberOfRollsTwo <= numberOfRollsOne)
+			// for (i = 1; i < document.getElementById("numberOfRollsOne").textContent.parseint; i++) {
+				// 	var displayBalanceTwo = document.getElementById("balanceTwo").textContent;
+				// 	var displayRollsTwo = document.getElementById("numberOfRollsTwo").textContent;
+				// 	numberOfRollsTwo = document.getElementById("numberOfRollsOne").textContent - 1
+				// 	var dice1 = Math.floor((Math.random()*6)+1)
+				// 	var dice2 = Math.floor((Math.random()*6)+1)
+
+				// 	if (dice1 + dice2 === 7 || dice1 + dice2 === 11) {
+				// 		balanceTwo = balanceTwo + 1;
+				// 	} else if (dice1 + dice2 === 2) {
+				// 		balanceTwo = balanceTwo -1;
+				// 	};
+
+				// 	displayBalanceTwo.innerHTML = balanceTwo;
+				// 	displayRollsTwo.innerHTML = numberOfRollsTwo;
+				// }
+	var displayBalanceTwo = document.getElementById("balanceTwo");
+	var displayRollsTwo = document.getElementById("numberOfRollsTwo");
+	numberOfRollsTwo = numberOfRollsOne + 1;
+	var dice1 = Math.floor((Math.random()*6)+1);
+	var dice2 = Math.floor((Math.random()*6)+1);
+
+	if (dice1 + dice2 === 7 || dice1 + dice2 === 11) {
+		balanceTwo = balanceTwo + 1;
+	} else if (dice1 + dice2 === 2) {
+		balanceTwo = balanceTwo - 1;
+	};
+
+	displayBalanceTwo.innerHTML = balanceTwo;
+	displayRollsTwo.innerHTML = numberOfRollsTwo;
 	}
-};
 
 function winCalculation() {
 	winning = balance - myBalance
